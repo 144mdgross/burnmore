9f83b304205066cd935d1fcbdffddec0bb487db2
144mdgross <144.mdgross@gmail.com>
2017-04-10 22:41:14 -0600

    read all phonographs and individual phonograph

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('../knex')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('phonographs')
+  .then(phone => {
+    res.render('index', {phone})
+  })
+});
+
+router.get('/:id', function(req, res, next) {
+  let id = +req.params.id
+  knex('phonographs')
+  .where('id', id)
+  .then(phone => {
+    res.render('indexId', {phone})
+  })
 });
 
 module.exports = router;
